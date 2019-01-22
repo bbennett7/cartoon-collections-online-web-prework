6739f4b073061ca5759318443e1ab017085609d2
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-22 05:01:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 def find_the_cheese(cheese_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_array.find do |type|
-    cheese_array.include?(type)
+    cheese_types.include?(type)
   end
 end
